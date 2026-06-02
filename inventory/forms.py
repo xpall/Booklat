1085d855de8f3f5cabde93f5e09bb9af09b1cbe3
xpall <contact@johnlloyd.dev>
2026-06-02 21:21:52 +0800

    Fix Date fields

--- inventory/forms.py
+++ inventory/forms.py
@@ -3,6 +3,11 @@ from .models import BookCopy
 
 
 class CopyForm(forms.ModelForm):
+    acquisition_date = forms.DateField(
+        required=False,
+        widget=forms.DateInput(attrs={"type": "date"}),
+    )
+
     class Meta:
         model = BookCopy
         fields = ["book", "acquisition_date", "shelf_location", "notes", "status"]
